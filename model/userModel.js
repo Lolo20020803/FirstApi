2070de8ec6cc8cf7e67ea21069dac7795a8d10da
Alex <alexsp2002@outlook.es>
2022-03-08 15:05:16 +0100

    cambios en el modelo para tener contraseñas cifradas

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { randomByte, pbkdf2Sync } = require('crypto');
 
 const { Schema } = mongoose;
+
 // Creation of mongoose schema
 const userSchema = new Schema({
   name: {
@@ -16,6 +18,32 @@ const userSchema = new Schema({
     unique: true,
     required: [true, 'Email required'],
   },
+  password: {
+    type: String,
+    unique: true,
+    required: [true, 'Password is required'],
+  },
+  saltPass: {
+    type: String,
+  },
 });
+
+function hashPasword(next) {
+  const user = this;
+
+  user.saltPass = randomByte(16).toString('hex');
+
+  if (user.isModified('password')) {
+    user.password = pbkdf2Sync(user.password, user.saltPass, 100000, 64, 'sha512');
+  }
+  return next();
+}
+function comparePass(pass) {
+  const user = this;
+  return user.password === pbkdf2Sync(pass, user.saltPass, 100000, 64, 'sha512');
+}
+
+userSchema.pre('save', hashPasword);
+userSchema.methods.comparePass = comparePass;
 
 module.exports = mongoose.model('User', userSchema);
